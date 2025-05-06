753c369c5fe7ca02d987a32982defbc390921846
Володимир <zeleneckijvolodimir2012@gmail.com>
2025-05-06 17:35:23 +0300

    # ctrl + c  ctrl + v + редаг

--- pythonProject2/main.py
+++ pythonProject2/main.py
@@ -3,32 +3,32 @@
 import random
 class Student:
  def __init__(self, name):
- self.name = name
- self.gladness = 50
- self.progress = 0
- self.alive = True
+     self.name = name
+     self.gladness = 50
+     self.progress = 0
+     self.alive = True
  def to_study(self):
- print("Time to study")
- self.progress += 0.12
- self.gladness -= 5
+     print("Time to study")
+     self.progress += 0.12
+     self.gladness -= 5
  def to_sleep(self):
- print("I will sleep")
- self.gladness += 3
+     print("I will sleep")
+     self.gladness += 3
  def to_chill(self):
- print("Rest time")
- self.gladness += 5
- self.progress -= 0.1
+     print("Rest time")
+     self.gladness += 5
+     self.progress -= 0.1
 
  def is_alive(self):
      if self.progress < -0.5:
          print("Cast out…")
-     self.alive = False
+         self.alive = False
      elif self.gladness <= 0:
-     print("Depression…")
-     self.alive = False
+         print("Depression…")
+         self.alive = False
      elif self.progress > 5:
-     print("Passed externally…")
-     self.alive = False
+        print("Passed externally…")
+         self.alive = False
 
      def end_of_day(self):
          print(f
